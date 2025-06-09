1d79efa8d098060d3eb6423cdf58d1b56086dbbe
gusals2025 <bau888a@gmail.com>
2025-06-09 23:47:50 +0900

    Add files via upload

---
+++ Project33.cpp
@@ -0,0 +1,21 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int scores[10];
+    int sum = 0;
+    double average;
+
+    srand((unsigned int)time(0));
+
+    for (int i = 0; i < 10; i++) {
+        scores[i] = rand() % 101;
+        sum += scores[i];
+    }
+
+    average = sum / 10.0;
+
+    printf("학생 성적 평균: %.2f\n", average);
+    return 0;
+}
